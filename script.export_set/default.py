645fe1243dc53e137ceb3b60e12992e90bee7a73
scott967 <scott967@users.noreply.github.com>
2025-10-05 14:34:42 -1000

    pep8 format / documentation

--- script.export_set/default.py
+++ script.export_set/default.py
@@ -32,6 +32,15 @@
 #
 #
 # pylint: disable=line-too-long,invalid-name
+""" Kodi program (script) addon to export video library set/collection
+data to "set.nfo" files in a user's MSIF folder tree.  When executed, a
+Kodi yes/no dialog is raised to determine if existing set.nfo file should
+be overwritten (updated).
+
+Raises:
+    ValueError: Custom handler for invalid or missing user MSIF  Generates
+    a Kodi LOGERROR and UI ok popup.
+"""
 
 import xml.etree.ElementTree as ET
 from pathlib import Path
@@ -43,15 +52,19 @@ import xbmcaddon
 import xbmcgui
 
 MSIF = None
+ADDON = xbmcaddon.Addon()
+ADDON_ID = ADDON.getAddonInfo('id')
+
+# get the Kodi user MSIF from Kodi settings.  If successful MSIF is valid Path object.
+# Note:  Path class will return '.' as path if no argument provided in init.
 try:
-    ADDON = xbmcaddon.Addon()
-    ADDON_ID = ADDON.getAddonInfo('id')
     MSIF = Path(simplejson.loads(xbmc.executeJSONRPC(
                 '{"jsonrpc":"2.0", "method":"Settings.GetSettingValue", "params":{"setting":"videolibrary.moviesetsfolder"}, "id":1}'))['result']['value'])
     if (MSIF is None) or (not MSIF.name):
         xbmcgui.Dialog().ok(ADDON_ID, ADDON.getLocalizedString(32001))
         MSIF = None
-        xbmc.log(f'{ADDON_ID} invalid or no movie set info folder', xbmc.LOGWARNING)
+        xbmc.log(f'{ADDON_ID} invalid or no movie set info folder',
+                 xbmc.LOGWARNING)
         raise ValueError
 except simplejson.JSONDecodeError:
     xbmcgui.Dialog().ok(ADDON_ID, ADDON.getLocalizedString(32001))
@@ -60,10 +73,11 @@ except simplejson.JSONDecodeError:
 except ValueError:
     xbmc.log(f'{ADDON_ID} unable to export', xbmc.LOGERROR)
 
-ELEMENTS = ['title', 'overview', 'originaltitle']
+ELEMENTS = ['title', 'overview', 'originaltitle'] #set/collection info to add to set.nfo
 
-def get_ET_trees(source:list[list], overwrite=False):
-    """Generate and save to MSIF nfo files for each row in source
+
+def get_ET_trees(source: list[list], overwrite=False):
+    """Generate and save to MSIF set.nfo files for each row in source
 
     Args:
         source (list[list]): A list of movie set info.  Each row is a list of set ELEMENTS
@@ -80,27 +94,31 @@ def get_ET_trees(source:list[list], overwrite=False):
         tree = ET.ElementTree(root)
         ET.indent(tree, space="\t", level=0)
         try:
-            sani_path:Path = sanitize_filepath((MSIF / row[1].replace('/', '_')), replacement_text='_', platform="auto", normalize=False)
-            sani_path.mkdir(exist_ok=True) #Path objects don't allow "/"
+            sani_path: Path = sanitize_filepath(
+                (MSIF / row[1].replace('/', '_')), replacement_text='_', platform="auto", normalize=False)
+            sani_path.mkdir(exist_ok=True)  # Path objects don't allow "/"
             if overwrite or not (sani_path / 'set.nfo').is_file():
-                tree.write(sani_path / 'set.nfo', encoding='utf-8', xml_declaration=True)
+                tree.write(sani_path / 'set.nfo',
+                           encoding='utf-8', xml_declaration=True)
         except IOError as err:
             xbmc.log(f'{ADDON_ID} Could not write set.nfo file due to {err}')
 
-def export_set_data(sif:Path=None):
+def export_set_data(sif: Path = None):
     """retrieves set data from library and creates set.nfo
 
     Args:
         sif (Path, optional): Path object for MSIF. Defaults to None.
     """
     if sif:
-        replace_nfo = xbmcgui.Dialog().yesno(ADDON_ID, ADDON.getLocalizedString(32004)) #overwrite yes/no
+        replace_nfo = xbmcgui.Dialog().yesno(
+            ADDON_ID, ADDON.getLocalizedString(32004))  # overwrite yes/no
         response = simplejson.loads(xbmc.executeJSONRPC(
-                '{"jsonrpc":"2.0", "method":"VideoLibrary.GetMovieSets", "params":{"properties":["title", "plot"]}, "id":1}'))
+            '{"jsonrpc":"2.0", "method":"VideoLibrary.GetMovieSets", "params":{"properties":["title", "plot"]}, "id":1}'))
         if ('result' in response) and ('sets' in response['result']):
-            lib_rows = [] #list of set property rows (1 row per set)
+            lib_rows = []  # list of set property rows (1 row per set)
             for i in range(response['result']['limits']['total']):
-                lib_rows.append([0, response['result']['sets'][i].get('label', ''), response['result']['sets'][i].get('plot', '')])
+                lib_rows.append([0, response['result']['sets'][i].get(
+                    'label', ''), response['result']['sets'][i].get('plot', '')])
             get_ET_trees(lib_rows, overwrite=replace_nfo)
 
 if __name__ == '__main__':
